3d85c8c0e4aae8d94b51f14a2784cea6cd5dcdc5
rising-sun-112 <asahi0803.ruru@gmail.com>
2021-02-13 00:35:37 +0900

    fourth

--- Assets/Script/ToScript/TOAIBattleReadyButton.cs
+++ Assets/Script/ToScript/TOAIBattleReadyButton.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class TOAIBattleReadyButton : MonoBehaviour
+public class ToAIBattleReadyButton : MonoBehaviour
 {
          public void OnClick() {
     Debug.Log("Button click!");
